ddb5470d7f078cde18425db5da366a588daacd0d
Dclimberxz <2986100385@qq.com>
2022-05-31 00:14:26 +0800

    侧面射击

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -10,8 +10,8 @@ class Settings:
 
         # 子弹设置
         self.bullet_speed = 1.0
-        self.bullet_width = 3
-        self.bullet_height = 15
+        self.bullet_width = 15
+        self.bullet_height = 3
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
